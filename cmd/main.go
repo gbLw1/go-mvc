b35a3234a5e0f4ddbbd33a842e91ce48d00a44a1
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-10-16 21:46:07 -0300

    .

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"go-mvc/pkg/controllers"
 	"go-mvc/pkg/initializers"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 )
 
 func init() {
 	initializers.LoadEnvVariables()
 	initializers.ConnectToDatabase()
+	initializers.SyncDB()
 }
 
 func main() {
-	fmt.Println("hello world")
+	engine := html.New("./pkg/views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Static("/", "./public")
+
+	app.Get("/", controllers.PostsGetAll)
+
+	app.Listen(":" + os.Getenv("PORT"))
 }
